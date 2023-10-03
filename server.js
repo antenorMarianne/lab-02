2a0e3b8e7406f68b83c0f00d373eab4662899c6c
Marianne Antenor <21480725@students.ltu.edu.au>
2023-10-03 17:30:38 +1100

    First commit

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 // Require the Express module
 const express = require('express');
+const child_process = require('child_process');
+
 // Create an Express web application
 const app = express();
-// Specify how to respond to GET /
+
+// Set handler for the index of the website
 app.get('/', (req, res) => {
-res.send('Your fortune is : Hello World! \n' + Date());
-});
+    // Run the system `fortune` command and respond with the message
+    child_process.exec('fortune', (error, message) => {
+    if(error === null) {
+    res.send(message);
+    } else {
+    res.send('Error: ' + error);
+    }
+    })
+    });
+
+// Specify how to respond to GET /
+//app.get('/', (req, res) => {
+    ''//res.send('Your fortune is : Hello World! \n' + Date());
+//});
+
 // Start listening for HTTP requests on port 3000
 app.listen(3000, () => {
-console.log('Server started');
+    console.log('Server started');
 });
